640ad40023c56fe3d4c9276cd7fdfb6f947e044e
andrestemmett <originsgrand@gmail.com>
2021-01-12 13:46:19 -0500

    heroku ready

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'mvz)m@c@pqkorp-@5pf_5ah#1@fj@4n3@6&gx3ubp8+i81vi7!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecomandre.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -120,6 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
